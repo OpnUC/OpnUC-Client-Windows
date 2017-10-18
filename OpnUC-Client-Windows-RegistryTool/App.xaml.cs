5e70e1553f07063697f1ff676b5060930d056f9a
Tatsuya Ueda <ml@tatsuya.info>
2017-10-18 13:02:52 +0900

    - Adding Registry Tool

---
+++ OpnUC-Client-Windows-RegistryTool/App.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Windows;
+
+namespace OpnUC_Client_Windows_RegistryTool
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+
+            if (e.Args.Length > 0)
+            {
+                string path_exe = String.Format(@"{0}", e.Args[0]);
+
+                RegisterUriSchemeShellHandler("tel", "Tel", path_exe);
+                MessageBox.Show("レジストリ登録が完了しました。");
+            }
+
+            this.Shutdown();
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="scheme"></param>
+        /// <param name="proto"></param>
+        /// <param name="path_exe"></param>
+        /// <param name="path_icon"></param>
+        private static void RegisterUriSchemeShellHandler(string scheme, string proto, string path_exe, string path_icon = null)
+        {
+
+            path_icon = path_icon ?? path_exe;
+
+            var KeyName = Microsoft.Win32.Registry.LocalMachine
+                .CreateSubKey("SOFTWARE")
+                .CreateSubKey("OpnUC")
+                .CreateSubKey("Client")
+                .CreateSubKey("Capabilities")
+                .ToString();
+
+            Microsoft.Win32.Registry.LocalMachine
+                .CreateSubKey("SOFTWARE")
+                .CreateSubKey("OpnUC")
+                .CreateSubKey("Client")
+                .CreateSubKey("Capabilities")
+                .SetValue("ApplicationName", "OpnUC Client");
+
+            Microsoft.Win32.Registry.LocalMachine
+                .CreateSubKey("SOFTWARE")
+                .CreateSubKey("OpnUC")
+                .CreateSubKey("Client")
+                .CreateSubKey("Capabilities")
+                .SetValue("ApplicationDescription", "OpnUC Client");
+
+            Microsoft.Win32.Registry.LocalMachine
+                .CreateSubKey("SOFTWARE")
+                .CreateSubKey("OpnUC")
+                .CreateSubKey("Client").CreateSubKey("Capabilities")
+                .CreateSubKey("URLAssociations")
+                .SetValue("tel", "OpnUC-Client.Url.tel");
+
+            Microsoft.Win32.Registry.LocalMachine
+                .CreateSubKey("SOFTWARE")
+                .CreateSubKey("RegisteredApplications")
+                .SetValue("OpnUC", "SOFTWARE\\OpnUC\\Client\\Capabilities");
+
+            var url = Microsoft.Win32.Registry.ClassesRoot
+                .CreateSubKey("OpnUC-Client.Url.tel");
+
+            url.SetValue("", string.Format("URL:{0} Protocol", proto));
+            url.SetValue("URL Protocol", "");
+            var key_deficon = url.CreateSubKey("DefaultIcon");
+            key_deficon.SetValue("", string.Format(@"""{0}""", path_icon));
+
+            // Executable Path
+            var key_command = url.CreateSubKey("shell").CreateSubKey("open").CreateSubKey("command");
+            key_command.SetValue("",
+                string.Format(@"""{0}"" ""%1""", path_exe)
+                );
+
+        }
+              
+    }
+
+}
